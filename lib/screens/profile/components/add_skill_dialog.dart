d7136fff21ca8b0927cb433b3d2df557246c4dd9
RuaAwaysa123 <113847802+RuaAwaysa123@users.noreply.github.com>
2023-10-22 23:59:37 +0300

    new profile update

---
+++ lib/screens/profile/components/add_skill_dialog.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:rive_animation/model/user.dart';
+import 'package:rive_animation/utils/user_preferences.dart';
+
+class AddSkillDialog extends StatefulWidget {
+  final Function(Skill) onSkillAdded;
+
+  AddSkillDialog({required this.onSkillAdded});
+
+  @override
+  _AddSkillDialogState createState() => _AddSkillDialogState();
+}
+
+class _AddSkillDialogState extends State<AddSkillDialog> {
+  String skillName = '';
+  int skillRating = 1; // Ensure skillRating is not nullable
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Add New Skill'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Skill Name'),
+            onChanged: (value) {
+              setState(() {
+                skillName = value;
+              });
+            },
+          ),
+          DropdownButton<int>(
+            value: skillRating ?? 1, // Ensure skillRating is not nullable
+            items: List.generate(5, (index) {
+              return DropdownMenuItem<int>(
+                value: index + 1,
+                child: Text('Rating: ${index + 1}'),
+              );
+            }),
+            onChanged: (value) {
+              setState(() {
+                skillRating = value ?? 1; // Ensure skillRating is not nullable
+              });
+            },
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: Text('Cancel'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        // TextButton(
+        //   child: Text('Add'),
+        //   onPressed: () {
+        //     if (skillName.isNotEmpty) {
+        //       final newSkill = Skill(name: skillName, rating: skillRating);
+        //       // Add the new skill to the user's skills list
+        //       // You need to implement the logic to add the skill to the user here
+        //       Navigator.of(context).pop();
+        //     }
+        //   },
+        // ),
+        TextButton(
+          child: Text('Add'),
+          onPressed: () {
+            if (skillName.isNotEmpty) {
+              final newSkill = Skill(name: skillName, rating: skillRating);
+              widget.onSkillAdded(newSkill); // Call the callback to add the skill
+              Navigator.of(context).pop();
+            }
+          },
+        ),
+
+      ],
+    );
+  }
+}
--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -10,6 +10,8 @@ import 'package:rive_animation/widget/numbers_widget.dart';
 import 'package:rive_animation/widget/profile_widget.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'components/add_skill_dialog.dart';
+
 class ProfilePage extends StatefulWidget {
   @override
   _ProfilePageState createState() => _ProfilePageState();
@@ -27,31 +29,26 @@ class _ProfilePageState extends State<ProfilePage> {
           body: ListView(
             physics: BouncingScrollPhysics(),
             children: [
-             // if (user != null) // Check if user is not null
-                ProfileWidget(
-                  imagePath: user.imagePath,
-                  onClicked: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => EditProfilePage()),
-                    );
-                  },
-                ),
+              ProfileWidget(
+                imagePath: user.imagePath,
+                onClicked: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => EditProfilePage()),
+                  );
+                },
+              ),
               const SizedBox(height: 24),
-            //  if (user != null) // Check if user is not null
-                buildName(user),
+              buildName(user),
               const SizedBox(height: 24),
               Center(child: buildUpgradeButton()),
               const SizedBox(height: 24),
               NumbersWidget(),
               const SizedBox(height: 10),
-              //if (user != null) // Check if user is not null
-            
               buildAboutme(user),
               const SizedBox(height: 30),
-                buildAbout(user),
-              // const SizedBox(height: 10),
-              //     buildSkills(user),
-
+              buildAbout(user),
+              const SizedBox(height: 10),
+              buildSkills(context, user),
             ],
           ),
         ),
@@ -95,118 +92,113 @@ class _ProfilePageState extends State<ProfilePage> {
       ],
     ),
   );
-}
 
-
-Widget buildAboutme(User user) => Container(
-  padding: EdgeInsets.symmetric(horizontal: 48),
-  child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        '',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-      ),
-      const SizedBox(height: 16),
-      Row(
-        children: [
-          Icon(FontAwesomeIcons.phone, size: 18 , color: Colors.blue),
-          SizedBox(width: 8),
-          Text(
-            user.phoneNumber,
-            style: TextStyle(fontSize: 16, height: 1.4),
-          ),
-        ],
-      ),
-      const SizedBox(height: 8),
-      Row(
-        children: [
-          Icon(FontAwesomeIcons.graduationCap, size: 18 , color: Colors.blue),
-          SizedBox(width: 8),
-          Text(
-            user.major,
-            style: TextStyle(fontSize: 16, height: 1.4),
-          ),
-        ],
-      ),
-      const SizedBox(height: 8),
-      // Row(
-      //   children: [
-      //     Icon(FontAwesomeIcons.envelope, size: 18),
-      //     SizedBox(width: 8),
-      //     Text(
-      //       user.email,
-      //       style: TextStyle(fontSize: 16, height: 1.4),
-      //     ),
-      //   ],
-      // ),
-      const SizedBox(height: 8),
-      Row(
-        children: [
-          Icon(FontAwesomeIcons.university, size: 18, color: Colors.blue),
-          SizedBox(width: 8),
-          Text(
-            user.university,
-            style: TextStyle(fontSize: 16, height: 1.4,),
-          ),
-        ],
-      ),
-      const SizedBox(height: 8),
-      Row(
-        children: [
-          Icon(FontAwesomeIcons.locationPinLock, size: 18,color: Colors.blue,),
-          SizedBox(width: 8),
-          Text(
-            user.location,
-            style: TextStyle(fontSize: 16, height: 1.4),
-          ),
-        ],
-      ),
-    ],
-  ),
-);
-Widget buildSkills(User user) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        'Skills',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-      ),
-      const SizedBox(height: 16),
-      for (var skill in user.skills)
+  Widget buildAboutme(User user) => Container(
+    padding: EdgeInsets.symmetric(horizontal: 48),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'About Me',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 16),
         Row(
           children: [
-            Text(skill.name),
-            // Spacer(),
-            const SizedBox(width: 30,),
-            buildSkillRating(skill.rating),
+            Icon(FontAwesomeIcons.phone, size: 18, color: Colors.blue),
+            SizedBox(width: 8),
+            Text(
+              user.phoneNumber,
+              style: TextStyle(fontSize: 16, height: 1.4),
+            ),
           ],
         ),
-      const SizedBox(height: 16),
-      buildAddSkillButton(), // Add the "Add Skill" button here
-    ],
+        const SizedBox(height: 8),
+        Row(
+          children: [
+            Icon(FontAwesomeIcons.graduationCap, size: 18, color: Colors.blue),
+            SizedBox(width: 8),
+            Text(
+              user.major,
+              style: TextStyle(fontSize: 16, height: 1.4),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+        Row(
+          children: [
+            Icon(FontAwesomeIcons.university, size: 18, color: Colors.blue),
+            SizedBox(width: 8),
+            Text(
+              user.university,
+              style: TextStyle(fontSize: 16, height: 1.4),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+        Row(
+          children: [
+            Icon(FontAwesomeIcons.locationPin, size: 18, color: Colors.blue),
+            SizedBox(width: 8),
+            Text(
+              user.location,
+              style: TextStyle(fontSize: 16, height: 1.4),
+            ),
+          ],
+        ),
+      ],
+    ),
   );
-}
 
-Widget buildAddSkillButton() => ButtonWidget(
-  text: 'Add New Skill',
-  onClicked: () {
-    // Implement functionality to add new skills here
-  },
-);
-Widget buildSkillRating(int rating) {
-  List<Widget> stars = [];
-  for (int i = 0; i < 5; i++) {
-    if (i < rating) {
-      stars.add(Icon(Icons.star, color: Colors.yellow));
-    } else {
-      stars.add(Icon(Icons.star_border, color: Colors.yellow));
-    }
+  Widget buildSkills(BuildContext context, User user) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 48),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Skills',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          for (var skill in user.skills)
+            Row(
+              children: [
+                Text(skill.name),
+                const SizedBox(width: 30),
+                buildSkillRating(skill.rating),
+              ],
+            ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => AddSkillDialog(
+                  onSkillAdded: (newSkill) {
+                    setState(() {
+                      user.skills.add(newSkill);
+                    });
+                  },
+                ),
+              );
+            },
+            child: Text('Add New Skill'),
+          )
+        ],
+      ),
+    );
   }
-  return Row(children: stars);
+
+  Widget buildSkillRating(int rating) {
+    List<Widget> stars = [];
+    for (int i = 0; i < 5; i++) {
+      if (i < rating) {
+        stars.add(Icon(Icons.star, color: Colors.yellow));
+      } else {
+        stars.add(Icon(Icons.star_border, color: Colors.yellow));
+      }
+    }
+    return Row(children: stars);
+  }
 }
-
-
-//-----------------------------------------------------------------
-
